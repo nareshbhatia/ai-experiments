a56ba3c72c524a890a0651f7f0a27002f87aa698
Naresh Bhatia <me@nareshbhatia.dev>
2024-08-18 23:03:44 -0400

    feat: add code-llama chat to home page

---
+++ apps/codegen-code-llama/src/app/api/chat/route.ts
@@ -0,0 +1,17 @@
+import { StreamingTextResponse, streamText } from 'ai';
+import { createOllama } from 'ollama-ai-provider';
+
+const ollama = createOllama();
+
+export async function POST(req: Request) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { messages } = await req.json();
+
+  const result = await streamText({
+    model: ollama('codellama:70b-instruct'),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    messages,
+  });
+
+  return new StreamingTextResponse(result.toAIStream());
+}
